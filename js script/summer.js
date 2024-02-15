fc9d5bcb9c809094eec65f9cc9369ba0e518f591
AbidShazid3 <abidshazid3@gmail.com>
2024-02-16 05:01:27 +0600

    update my changes

--- js script/summer.js
+++ js script/summer.js
@@ -7,15 +7,15 @@ const cards = document.querySelectorAll('.card');
 let titleCount = 1;
 let totalPrice = 0;
 
-for(const card of cards){
-    card.addEventListener('click', function(){
+for (const card of cards) {
+    card.addEventListener('click', function () {
         // get the title
         const title = card.querySelector('h3');
         const titleText = title.innerText;
-        
+
         // to add the titleText to the new p
         const titleContainerShow = document.getElementById('title-container');
-        
+
         const p = document.createElement('p');
         p.innerText = titleCount + '. ' + titleText;
         titleContainerShow.appendChild(p);
@@ -24,7 +24,7 @@ for(const card of cards){
         // get the price
         const price = card.querySelector('span');
         const priceText = parseFloat(price.innerText.split(' ')[1]);
-        
+
         // calculate price
         totalPrice = totalPrice + priceText;
         document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
@@ -32,24 +32,19 @@ for(const card of cards){
 }
 
 const btn = document.getElementById('apply-btn');
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
     // get the input field value
     const inputField = document.getElementById('input-field');
     const inputFieldText = inputField.value;
     const inputFieldTextCode = inputFieldText.split(' ').join('').toUpperCase();
     console.log(typeof inputFieldTextCode);
-    if(totalPrice >= 200){
-        if(inputFieldTextCode === 'SELL200'){
-            const discountElement = document.getElementById('discountPrice');
-            const totalDiscount = totalPrice * 0.2;
-            discountElement.innerText = totalDiscount.toFixed(2);
-        }
-        else{
-            alert('invalid code');
-        }
+    if (totalPrice >= 200 && inputFieldTextCode === 'SELL200') {
+        const discountElement = document.getElementById('discountPrice');
+        const totalDiscount = totalPrice * 0.2;
+        discountElement.innerText = totalDiscount.toFixed(2);
     }
-    else{
+    else {
         alert('spent at list $200');
     }
-    
+
 })
